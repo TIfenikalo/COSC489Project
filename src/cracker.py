5f3faa0db14154576591b1f30d7c9a44712a2567
Titorian <115482369+Titorian@users.noreply.github.com>
2025-05-19 14:05:18 -0400

    Create cracker.py

---
+++ src/cracker.py
@@ -0,0 +1,6 @@
+from utils import run_command
+
+def crack_password(filename):
+    print("Step 6: Cracking password with aircrack-ng and rockyou.txt...")
+    cap_file = f"{filename}-01.cap"
+    run_command(f"sudo aircrack-ng {cap_file} -w /usr/share/wordlists/rockyou.txt")
