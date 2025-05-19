8803e548ffda8f87a377e8494824aca8db1317ee
Titorian <115482369+Titorian@users.noreply.github.com>
2025-05-19 14:03:25 -0400

    Create monitor.py

---
+++ src/monitor.py
@@ -0,0 +1,12 @@
+from utils import run_command
+
+def start_monitor():
+    print("Step 1: Killing processes that may interfere...")
+    run_command("sudo airmon-ng check kill")
+    
+    print("Step 2: Starting monitor mode on wlan0...")
+    run_command("sudo airmon-ng start wlan0")
+
+def stop_monitor():
+    print("Step 5: Stopping monitor mode...")
+    run_command("sudo airmon-ng stop wlan0")
