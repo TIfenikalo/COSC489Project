57e4dfe2abef9df58b54a0a90bcc60f42f335e35
Titorian <115482369+Titorian@users.noreply.github.com>
2025-05-19 14:01:33 -0400

    Delete src/auditcomponents/handshakecapture.py

--- src/auditcomponents/handshakecapture.py
+++
@@ -1,5 +0,0 @@
-import subprocess # Used to run terminal commands in python
-
-def capture_handshake(interface, bssid, channel, output_prefix):
-    subprocess.run(['airodump-ng', '--bssid', bssid, '-c', channel, '-w', output_prefix, interface])
-
