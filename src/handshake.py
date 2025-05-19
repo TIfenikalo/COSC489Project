2823db716c3d9a8d6d111daf2b8dfc8641c40147
Titorian <115482369+Titorian@users.noreply.github.com>
2025-05-19 14:12:07 -0400

    Update handshake.py

--- src/handshake.py
+++ src/handshake.py
@@ -1,3 +1,4 @@
+#Once the access point is chosen this is to scan for any handshakes
 def capture_handshake(bssid, channel, filename):
     print("Step 4: Capturing handshake. Press CTRL+C when handshake is captured.")
     command = f"sudo airodump-ng -w {filename} -c {channel} --bssid {bssid} wlan0mon"
